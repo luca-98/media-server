edbea73aa7cc9c4939a044d84f6cefcc56f0e1c6
Luca <54134832+luca-98@users.noreply.github.com>
2022-03-07 16:00:04 +0700

    update

--- test-worker.js
+++
@@ -1,7 +0,0 @@
-const mediasoup = require("mediasoup");
-async function startWorker() {
-  let worker = await mediasoup.createWorker();
-  console.log("-- mediasoup worker start. --".worker);
-}
-
-startWorker();
